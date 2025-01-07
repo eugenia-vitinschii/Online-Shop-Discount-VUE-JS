2375c4cac8a1eff53af7dc3b08606b184979295c
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-01-07 16:07:01 +0200

    edit select options for energyEfficiencyClass, colors for add & update view (admin)

--- src/views/Admin/AddProductView.vue
+++ src/views/Admin/AddProductView.vue
@@ -8,152 +8,188 @@
       </div>
       <div class="add__content">
         <form action="" class="add__form">
-          <p class="heading">Date generale</p>
-          <the-input
-            :label="'Introduceți id'"
-            :placeholder="'Introduceți id'"
-            v-model.trim:value="postProducts.id"
-          />
-          <the-input
-            :label="'Introduceți COD'"
-            :placeholder="'DMB383292PP'"
-            v-model:value.trim="postProducts.productCode"
-          />
-          <the-input
-            :label="'Introduceți url pentru imagine'"
-            :placeholder="'//http'"
-            v-model:value.trim="postProducts.img"
-          />
-          <the-input
-            :label="'Introduceți denumirea pentru produs'"
-            :placeholder="'Mașină de spălat frontală Indesit'"
-            v-model:value.trim="postProducts.productName"
-          />
-          <the-input
-            :label="'Introduceți prețul produsului'"
-            :placeholder="'4999'"
-            v-model:value.trim="postProducts.price"
-          />
-          <the-input
-            :label="'Introduceți procentul de reducere ( fără %)'"
-            :placeholder="'17'"
-            v-model:value.trim="postProducts.discount"
-          />
-          <p class="heading">Informația despre produs</p>
-          <!-- brand -->
-          <div class="input__wrapper">
-            <label for="brand">Brand</label>
-            <select name="brand" id="brand" v-model="postProducts.brand">
-              <option value="AEG">AEG</option>
-              <option value="Indesit">Indesit</option>
-              <option value="Beko">Beko</option>
-              <option value="Whirlpool">Whirlpool</option>
-              <option value="Samsung">Samsung</option>
-              <option value="Electrolux">Electrolux</option>
-              <option value="Candy">Candy</option>
-            </select>
+          <!-- Date generale -->
+          <div class="add__form-item">
+            <p class="heading">Date generale</p>
+            <the-input
+              :label="'Introduceți id'"
+              :placeholder="'Introduceți id'"
+              v-model.trim:value="postProducts.id"
+            />
+            <the-input
+              :label="'Introduceți COD'"
+              :placeholder="'DMB383292PP'"
+              v-model:value.trim="postProducts.productCode"
+            />
+            <the-input
+              :label="'Introduceți url pentru imagine'"
+              :placeholder="'//http'"
+              v-model:value.trim="postProducts.img"
+            />
+            <the-input
+              :label="'Introduceți denumirea pentru produs'"
+              :placeholder="'Mașină de spălat frontală Indesit'"
+              v-model:value.trim="postProducts.productName"
+            />
+            <the-input
+              :label="'Introduceți prețul produsului'"
+              :placeholder="'4999'"
+              v-model:value.trim="postProducts.price"
+            />
+            <the-input
+              :label="'Introduceți procentul de reducere ( fără %)'"
+              :placeholder="'17'"
+              v-model:value.trim="postProducts.discount"
+            />
+          </div>
+          <!-- Informația despre produs -->
+          <div class="add__form-item">
+            <p class="heading">Informația despre produs</p>
+            <!-- brand -->
+            <div class="input__wrapper">
+              <label for="brand">Brand</label>
+              <select name="brand" id="brand" v-model="postProducts.brand">
+                <option value="AEG">AEG</option>
+                <option value="Indesit">Indesit</option>
+                <option value="Beko">Beko</option>
+                <option value="Whirlpool">Whirlpool</option>
+                <option value="Samsung">Samsung</option>
+                <option value="Electrolux">Electrolux</option>
+                <option value="Candy">Candy</option>
+              </select>
+            </div>
+
+            <the-input
+              :label="'Consum de apă în program, l/ciclu'"
+              :placeholder="'20'"
+              v-model:value.trim="postProducts.waterConsumption"
+            />
+
+            <div class="input__wrapper">
+              <label for="energyEfficiencyClass"
+                >Clasa de eficienţă energetică</label
+              >
+              <select
+                name="energyEfficiencyClass"
+                id="energyEfficiencyClass"
+                v-model="postProducts.energyEfficiencyClass"
+              >
+                <option value="A">A</option>
+                <option value="A++">A++</option>
+                <option value="A+++">A+++</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+              </select>
+            </div>
+            <div class="input__wrapper">
+              <label for="type">Tip mașină de spălat</label>
+              <select name="type" id="type" v-model="postProducts.type">
+                <option value="cu încărcare frontală">
+                  cu încărcare frontală
+                </option>
+                <option value="verticală">verticală</option>
+              </select>
+            </div>
+            <the-input
+              :label="'Viteza maximă de centrifugare, rpm'"
+              :placeholder="'1000'"
+              v-model:value.trim="postProducts.spinSpeed"
+            />
+            <the-input
+              :label="'Capacitate de încărcare, kg'"
+              :placeholder="'5'"
+              v-model:value.trim="postProducts.loadCapacity"
+            />
+          </div>
+          <!-- Caracteristici -->
+          <div class="add__form-item">
+            <p class="heading">Caracteristici</p>
+            <the-input
+              :label="'Nivel de zgomot pentru centrifugare, dB'"
+              :placeholder="'76'"
+              v-model:value.trim="postProducts.noiseLevelCentrifugation"
+            />
+            <the-input
+              :label="'Diapazonul nivelului de zgomot la spălare, dB'"
+              :placeholder="'56-60'"
+              v-model:value.trim="postProducts.noiseLevelWashing"
+            />
+            <the-input
+              :label="'Tip control'"
+              :placeholder="'electronic'"
+              v-model:value.trim="postProducts.typeControl"
+            />
+            <div class="input__wrapper">
+              <label for="typeControl">Tip control</label>
+              <select
+                name="typeControl"
+                id="typeControl"
+                v-model="postProducts.typeControl"
+              >
+                <option value="electronic">electronic</option>
+                <option value="senzor">senzor</option>
+                <option value="mecanic">mecanic</option>
+              </select>
+            </div>
+            <the-input
+              :label="'Numărul de programe'"
+              :placeholder="'16'"
+              v-model:value.trim="postProducts.numberOfPrograms"
+            />
+          </div>
+          <!-- Dimensiuni -->
+          <div class="add__form-item">
+            <p class="heading">Dimensiuni</p>
+            <the-input
+              :label="'Greutate în ambalaj, kg'"
+              :placeholder="'63'"
+              v-model:value.trim="postProducts.weightInPackage"
+            />
+            <the-input
+              :label="'Greutate, kg'"
+              :placeholder="'61'"
+              v-model:value.trim="postProducts.weight"
+            />
+            <the-input
+              :label="'Dimensiuni (ÎxLxA), cm'"
+              :placeholder="'85x60x55'"
+              v-model:value.trim="postProducts.depth"
+            />
+            <p class="heading">Informaţii generale</p>
+            <div class="input__wrapper">
+              <label for="color">Culoare</label>
+              <select name="color" id="color" v-model="postProducts.color">
+                <option value="alb">alb</option>
+                <option value="sur">sur</option>
+                <option value="negru">negru</option>
+              </select>
+            </div>
+            <div class="input__wrapper">
+              <label for="country">Ţara de asamblare</label>
+              <select
+                name="country"
+                id="country"
+                v-model="postProducts.countryOfAssembly"
+              >
+                <option value="Polonia">Polonia</option>
+                <option value="România">România</option>
+                <option value="Slovenia">Slovenia</option>
+                <option value="Turcia">Turcia</option>
+                <option value="China">China</option>
+                <option value="Italia">Italia</option>
+                <option value="USA">USA</option>
+              </select>
+            </div>
+            <the-input
+              :label="'Garanţie, luni'"
+              :placeholder="'36'"
+              v-model:value.trim="postProducts.guarantee"
+            />
           </div>
 
-          <the-input
-            :label="'Consum de apă în program, l/ciclu'"
-            :placeholder="'20'"
-            v-model:value.trim="postProducts.waterConsumption"
-          />
-
-          <div class="input__wrapper">
-            <label for="energyEfficiencyClass"
-              >Clasa de eficienţă energetică</label
-            >
-            <select
-              name="energyEfficiencyClass"
-              id="energyEfficiencyClass"
-              v-model="postProducts.energyEfficiencyClass"
-            >
-              <option value="A++">A++</option>
-              <option value="A+++">A+++</option>
-            </select>
-          </div>
-          <div class="input__wrapper">
-            <label for="type">Tip mașină de spălat</label>
-            <select name="type" id="type" v-model="postProducts.type">
-              <option value="cu încărcare frontală">
-                cu încărcare frontală
-              </option>
-              <option value="verticală">verticală</option>
-            </select>
-          </div>
-          <the-input
-            :label="'Viteza maximă de centrifugare, rpm'"
-            :placeholder="'1000'"
-            v-model:value.trim="postProducts.spinSpeed"
-          />
-          <the-input
-            :label="'Capacitate de încărcare, kg'"
-            :placeholder="'5'"
-            v-model:value.trim="postProducts.loadCapacity"
-          />
-          <p class="heading">Caracteristici</p>
-          <the-input
-            :label="'Nivel de zgomot pentru centrifugare, dB'"
-            :placeholder="'76'"
-            v-model:value.trim="postProducts.noiseLevelCentrifugation"
-          />
-          <the-input
-            :label="'Diapazonul nivelului de zgomot la spălare, dB'"
-            :placeholder="'56-60'"
-            v-model:value.trim="postProducts.noiseLevelWashing"
-          />
-          <the-input
-            :label="'Tip control'"
-            :placeholder="'electronic'"
-            v-model:value.trim="postProducts.typeControl"
-          />
-                  <div class="input__wrapper">
-            <label for="typeControl">Tip control</label>
-            <select name="typeControl" id="typeControl" v-model="postProducts.typeControl">
-              <option value="electronic">
-                electronic
-              </option>
-              <option value="verticală">verticală</option>
-            </select>
-          </div>
-          <the-input
-            :label="'Numărul de programe'"
-            :placeholder="'16'"
-            v-model:value.trim="postProducts.numberOfPrograms"
-          />
-          <p class="heading">Dimensiuni</p>
-          <the-input
-            :label="'Greutate în ambalaj, kg'"
-            :placeholder="'63'"
-            v-model:value.trim="postProducts.weightInPackage"
-          />
-          <the-input
-            :label="'Adâncime, cm'"
-            :placeholder="'43'"
-            v-model:value.trim="postProducts.depth"
-          />
-          <the-input
-            :label="'Greutate, kg'"
-            :placeholder="'61'"
-            v-model:value.trim="postProducts.weight"
-          />
-          <p class="heading">Informaţii generale</p>
-          <the-input
-            :label="'Culoare'"
-            :placeholder="' alb'"
-            v-model:value.trim="postProducts.color"
-          />
-          <the-input
-            :label="'Ţara de asamblare'"
-            :placeholder="'Polonia'"
-            v-model:value.trim="postProducts.countryOfAssembly"
-          />
-          <the-input
-            :label="'Garanţie, luni'"
-            :placeholder="'36'"
-            v-model:value.trim="postProducts.guarantee"
-          />
           <the-button class="green-button" @click.prevent="addPostData()">
             Salvează
           </the-button>
--- src/views/Admin/UpdateView.vue
+++ src/views/Admin/UpdateView.vue
@@ -3,127 +3,187 @@
   <div class="container">
     <div class="update">
       <div class="update__title">
-        <p class="heading">
-          Editeaza produsul   {{ products.productName }}
-        </p>
+        <p class="heading">Editeaza produsul {{ products.productName }}</p>
       </div>
       <div class="update__content">
         <form class="update__form">
-          <p class="heading">Date generale</p>
-          <the-input
-            :label="'Introduceți id'"
-            :placeholder="'Introduceți id'"
-            v-model:value.trim="products.id"
-          />
-          <the-input
-            :label="'Introduceți COD'"
-            :placeholder="'DMB383292PP'"
-            v-model:value.trim="products.productCode"
-          />
-          <the-input
-            :label="'Introduceți url pentru imagine'"
-            :placeholder="'//http'"
-            v-model:value.trim="products.img"
-          />
-          <the-input
-            :label="'Introduceți denumirea pentru produs'"
-            :placeholder="'Mașină de spălat frontală Indesit'"
-            v-model:value.trim="products.productName"
-          />
-          <the-input
-            :label="'Introduceți prețul produsului'"
-            :placeholder="'4999'"
-            v-model:value.trim="products.price"
-          />
-          <the-input
-            :label="'Introduceți procentul de reducere ( fără %)'"
-            :placeholder="'17'"
-            v-model:value.trim="products.discount"
-          />
-          <p class="heading">Informația despre produs</p>
-          <the-input
-            :label="'Introduceți denumirea brandului pentru produs'"
-            :placeholder="'Indesit'"
-            v-model:value.trim="products.brand"
-          />
-          <the-input
-            :label="'Consum de apă în program, l/ciclu'"
-            :placeholder="'20'"
-            v-model:value.trim="products.waterConsumption"
-          />
-          <the-input
-            :label="'Clasa de eficienţă energetică'"
-            :placeholder="'A++ / E (EU standard)'"
-            v-model:value.trim="products.energyEfficiencyClass"
-          />
-          <the-input
-            :label="'Tip mașină de spălat'"
-            :placeholder="'cu încărcare frontală'"
-            v-model:value.trim="products.type"
-          />
-          <the-input
-            :label="'Viteza maximă de centrifugare, rpm'"
-            :placeholder="'1000'"
-            v-model:value.trim="products.spinSpeed"
-          />
-          <the-input
-            :label="'Capacitate de încărcare, kg'"
-            :placeholder="'5'"
-            v-model:value.trim="products.loadCapacity"
-          />
-          <p class="heading">Caracteristici</p>
-          <the-input
-            :label="'Nivel de zgomot pentru centrifugare, dB'"
-            :placeholder="'76'"
-            v-model:value.trim="products.noiseLevelCentrifugation"
-          />
-          <the-input
-            :label="'Diapazonul nivelului de zgomot la spălare, dB'"
-            :placeholder="'56-60'"
-            v-model:value.trim="products.noiseLevelWashing"
-          />
-          <the-input
-            :label="'Tip control'"
-            :placeholder="'electronic'"
-            v-model:value.trim="products.typeControl"
-          />
-          <the-input
-            :label="'Numărul de programe'"
-            :placeholder="'16'"
-            v-model:value.trim="products.numberOfPrograms"
-          />
-          <p class="heading">Dimensiuni</p>
-          <the-input
-            :label="'Greutate în ambalaj, kg'"
-            :placeholder="'63'"
-            v-model:value.trim="products.weightInPackage"
-          />
-          <the-input
-            :label="'Adâncime, cm'"
-            :placeholder="'43'"
-            v-model:value.trim="products.depth"
-          />
-          <the-input
-            :label="'Greutate, kg'"
-            :placeholder="'61'"
-            v-model:value.trim="products.weight"
-          />
-          <p lass="heading">Informaţii generale</p>
-          <the-input
-            :label="'Culoare'"
-            :placeholder="' alb'"
-            v-model:value.trim="products.color"
-          />
-          <the-input
-            :label="'Ţara de asamblare'"
-            :placeholder="'Polonia'"
-            v-model:value.trim="products.countryOfAssembly"
-          />
-          <the-input
-            :label="'Garanţie, luni'"
-            :placeholder="'36'"
-            v-model:value.trim="products.guarantee"
-          />
+          <!-- Date generale -->
+          <div class="update__form-item">
+            <p class="heading">Date generale</p>
+            <the-input
+              :label="'Introduceți id'"
+              :placeholder="'Introduceți id'"
+              v-model:value.trim="products.id"
+            />
+            <the-input
+              :label="'Introduceți COD'"
+              :placeholder="'DMB383292PP'"
+              v-model:value.trim="products.productCode"
+            />
+            <the-input
+              :label="'Introduceți url pentru imagine'"
+              :placeholder="'//http'"
+              v-model:value.trim="products.img"
+            />
+            <the-input
+              :label="'Introduceți denumirea pentru produs'"
+              :placeholder="'Mașină de spălat frontală Indesit'"
+              v-model:value.trim="products.productName"
+            />
+            <the-input
+              :label="'Introduceți prețul produsului'"
+              :placeholder="'4999'"
+              v-model:value.trim="products.price"
+            />
+            <the-input
+              :label="'Introduceți procentul de reducere ( fără %)'"
+              :placeholder="'17'"
+              v-model:value.trim="products.discount"
+            />
+          </div>
+          <!-- Informația despre produs -->
+          <div class="update__form-item">
+            <p class="heading">Informația despre produs</p>
+            <div class="input__wrapper">
+              <label for="brand">Brand</label>
+              <select name="brand" id="brand" v-model="products.brand">
+                <option value="AEG">AEG</option>
+                <option value="Indesit">Indesit</option>
+                <option value="Beko">Beko</option>
+                <option value="Whirlpool">Whirlpool</option>
+                <option value="Samsung">Samsung</option>
+                <option value="Electrolux">Electrolux</option>
+                <option value="Candy">Candy</option>
+              </select>
+            </div>
+            <the-input
+              :label="'Consum de apă în program, l/ciclu'"
+              :placeholder="'20'"
+              v-model:value.trim="products.waterConsumption"
+            />
+
+            <div class="input__wrapper">
+              <label for="energyEfficiencyClass"
+                >Clasa de eficienţă energetică</label
+              >
+              <select
+                name="energyEfficiencyClass"
+                id="energyEfficiencyClass"
+                v-model="products.energyEfficiencyClass"
+              >
+                <option value="A">A</option>
+                <option value="A++">A++</option>
+                <option value="A+++">A+++</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+              </select>
+            </div>
+            <div class="input__wrapper">
+              <label for="type">Tip mașină de spălat</label>
+              <select name="type" id="type" v-model="products.type">
+                <option value="cu încărcare frontală">
+                  cu încărcare frontală
+                </option>
+                <option value="verticală">verticală</option>
+              </select>
+            </div>
+            <the-input
+              :label="'Viteza maximă de centrifugare, rpm'"
+              :placeholder="'1000'"
+              v-model:value.trim="products.spinSpeed"
+            />
+            <the-input
+              :label="'Capacitate de încărcare, kg'"
+              :placeholder="'5'"
+              v-model:value.trim="products.loadCapacity"
+            />
+          </div>
+          <!-- Caracteristici -->
+          <div class="update__form-item">
+            <p class="heading">Caracteristici</p>
+            <the-input
+              :label="'Nivel de zgomot pentru centrifugare, dB'"
+              :placeholder="'76'"
+              v-model:value.trim="products.noiseLevelCentrifugation"
+            />
+            <the-input
+              :label="'Diapazonul nivelului de zgomot la spălare, dB'"
+              :placeholder="'56-60'"
+              v-model:value.trim="products.noiseLevelWashing"
+            />
+            <div class="input__wrapper">
+              <label for="typeControl">Tip control</label>
+              <select
+                name="typeControl"
+                id="typeControl"
+                v-model="products.typeControl"
+              >
+                <option value="electronic">electronic</option>
+                <option value="senzor">senzor</option>
+                <option value="mecanic">mecanic</option>
+              </select>
+            </div>
+            <the-input
+              :label="'Numărul de programe'"
+              :placeholder="'16'"
+              v-model:value.trim="products.numberOfPrograms"
+            />
+          </div>
+          <!-- Dimensiuni -->
+          <div class="update__form-item">
+            <p class="heading">Dimensiuni</p>
+            <the-input
+              :label="'Greutate în ambalaj, kg'"
+              :placeholder="'63'"
+              v-model:value.trim="products.weightInPackage"
+            />
+            <the-input
+              :label="'Dimensiuni (ÎxLxA), cm'"
+              :placeholder="'85x60x55'"
+              v-model:value.trim="products.depth"
+            />
+            <the-input
+              :label="'Greutate, kg'"
+              :placeholder="'61'"
+              v-model:value.trim="products.weight"
+            />
+            <!-- Informaţii generale -->
+            <div class="update__form-item">
+              <p lass="heading">Informaţii generale</p>
+              <div class="input__wrapper">
+                <label for="color">Culoare</label>
+                <select name="color" id="color" v-model="products.color">
+                  <option value="alb">alb</option>
+                  <option value="sur">sur</option>
+                  <option value="negru">negru</option>
+                </select>
+              </div>
+              <div class="input__wrapper">
+                <label for="country">Ţara de asamblare</label>
+                <select
+                  name="country"
+                  id="country"
+                  v-model="products.countryOfAssembly"
+                >
+                  <option value="Polonia">Polonia</option>
+                  <option value="România">România</option>
+                  <option value="Slovenia">Slovenia</option>
+                  <option value="Turcia">Turcia</option>
+                  <option value="China">China</option>
+                  <option value="Italia">Italia</option>
+                  <option value="USA">USA</option>
+                </select>
+              </div>
+              <the-input
+                :label="'Garanţie, luni'"
+                :placeholder="'36'"
+                v-model:value.trim="products.guarantee"
+              />
+            </div>
+          </div>
           <the-button class="green-button" @click="updateItem()">
             Salvează
           </the-button>
@@ -132,7 +192,6 @@
           >
         </form>
       </div>
- 
     </div>
   </div>
   <message-component :message="message">Edited!</message-component>
@@ -140,20 +199,17 @@
  
 
 <script setup>
-
 import TheButton from "@/components/TheButton.vue";
 import TheInput from "@/components/TheInput.vue";
 import AdminHeader from "@/components/Admin/AdminHeader.vue";
 import MessageComponent from "@/components/Admin/MessageComponent.vue";
-
 
 import { useProductStore } from "@/stores/product";
 import { storeToRefs } from "pinia";
 import { onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { defineOptions } from "vue";
-import {ref} from 'vue';
-
+import { ref } from "vue";
 
 defineOptions({
   name: "UpdateView",
@@ -170,7 +226,7 @@ const route = useRoute();
 
 const id = route.params.id;
 
-let message = ref (false) ;
+let message = ref(false);
 console.log(message);
 
 function updateItem() {
