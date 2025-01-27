919dbf2ff9be7cc128f7845b2025b89dbf70994d
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-01-27 17:19:52 +0200

    added new img for bg, edit scss rules for promo section

--- src/sections/PromoSection.vue
+++ src/sections/PromoSection.vue
@@ -8,9 +8,25 @@
             <p class="body-text">Alege cele mai bune oferte, doar la Discount!</p>
          </div>
          <div class="bustle__time">
-            <p class="heading">
-               8 days
-            </p>
+            <div class="bustle__time-wrapper">
+               <div class="bustle__time-item">
+                  <p class="heading">08</p>
+                  <p class="subheading">Zile</p>
+               </div>
+              <div class="bustle__time-item">
+                  <p class="heading">05</p>
+                  <p class="subheading">Ore</p>
+               </div>
+               <div class="bustle__time-item">
+                  <p class="heading">34</p>
+                  <p class="subheading">Minute</p>
+               </div>
+               <div class="bustle__time-item">
+                  <p class="heading">06</p>
+                  <p class="subheading">Secunde</p>
+               </div>
+            </div>
+           
          </div> 
          </div>
       </div>
