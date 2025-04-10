56355d21405308399a25b8c92860adff8f9ad35e
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-04-10 14:07:22 +0300

    refactor pinia store/product to product folder and actions, getters, index, state (js) files

---
+++ src/stores/product/state.js
@@ -0,0 +1,14 @@
+//pinia
+
+
+export const  state = () => ({
+   products: [],
+   user: {
+     cart: [],
+     favorite: [],
+   },
+   checked: [],
+   ascending: true,
+   promo: true,
+   searchValue: '',
+})
