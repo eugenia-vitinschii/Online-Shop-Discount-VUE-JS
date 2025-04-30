7dda55a5957a7a0ca8df3c735ebe0531db3c3e51
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-04-30 14:59:26 +0300

    edit product model to ts lang

---
+++ frontend/src/stores/brands.ts
@@ -0,0 +1,46 @@
+//brand.ts store
+
+import { defineStore } from "pinia";
+import axios from "axios";
+import router from "@/router/index";
+
+const baseUrl = "http://localhost:3001";
+
+export interface Brand {
+  id: number;
+  name: String;
+  img: String;
+  brand: String;
+}
+
+export const useBrandsStore = defineStore("brands", {
+  state: () => ({
+    brands: [] as Brand[],
+  }),
+
+actions:{
+// get all breands from json
+  async fetchBrands(): Promise<void> {
+    try {
+      const response = await axios.get<Brand[]>(`${baseUrl}/brands`);
+      this.brands = response.data;
+    } catch (error) {
+        alert(error);
+        console.log(error);
+    }
+  },
+  // add new brand in db.json
+  async createBrand(payload: Omit<Brand, 'id'>): Promise<void> {
+    try {
+      const { data } = await axios.post<Brand>(`${baseUrl}/brands`, payload);
+      this.brands.push(data);
+      alert("Produsul a fost creat cu succes");
+    } catch (error) {
+        alert(error);
+      } finally {
+        router.push({ path: "/admin/panel" });
+      }
+    },
+  },
+});
+
