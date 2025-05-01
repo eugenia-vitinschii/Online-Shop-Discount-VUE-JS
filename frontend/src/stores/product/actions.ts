a0cf205375e8779e3b0e16ffe57066e4571c85e0
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-05-01 17:04:14 +0300

    improve actions.js to actions.ts, edit all functions

--- frontend/src/stores/product/actions.ts
+++ frontend/src/stores/product/actions.ts
@@ -6,14 +6,14 @@ import router from "@/router/index";
 
 //product
 import type { ProductState } from './state'
-import { Product, type ProductData } from "@/models/product"
+import { Product} from "@/models/product"
 import type { CartItem } from "./state";
 //base url
 const baseUrl = "http://localhost:3001";
 
 export const actions= {
 //fetch products
-   async fetchProducts(this: ProductState & { products: Product[]}): Promise<void> {
+   async fetchProducts(this: ProductState ): Promise<void> {
      try {
        const response = await axios.get(`${baseUrl}/products`);
        this.products = response.data.map((item:Record<string, any>) => new Product(item));
@@ -23,7 +23,7 @@ export const actions= {
      }
    },
 //create product
-   async createProduct(this: ProductState & { products: Product[]}, payload:Record<string, any>): Promise<void> {
+   async createProduct(this: ProductState, payload:Record<string, any>): Promise<void> {
      try {
        const newProduct = new Product(payload);
        const { data } = await axios.post(`${baseUrl}/products`, newProduct);
@@ -36,7 +36,7 @@ export const actions= {
      }
    },
 // delete product in db.json
-   async deleteProducts(this: ProductState & { products: Product[]}, id: string): Promise<boolean> {
+   async deleteProducts(this: ProductState , id: string): Promise<boolean> {
      try {
        await axios.delete(`${baseUrl}/products/${id}`);
        this.products = this.products.filter((prev) => prev.id !== id);
@@ -81,38 +81,38 @@ export const actions= {
      this.user.cart = this.user.cart.filter((item) => item.id !== id);
      localStorage.setItem("cart", JSON.stringify(this.user.cart))
    },
-   getSum() {
+   getSum(this: ProductState):number {
     return this.user.cart.reduce((sum, item) => {
       const itemTotal = Number(item.price) * (item.quantity || 1);
       return sum + itemTotal;
     }, 0);
   },
-   getSavedMoney() {
+   getSavedMoney(this: ProductState):number {
     return this.user.cart.reduce((sum, item) =>{
         const savedTotal = Number(item.savedMoney) * (item.quantity || 1);
         return sum + savedTotal
     }, 0);
    },
-   getDiscountPrice(a) {
-     return (b) => {
+   getDiscountPrice(a:number): (b: number) => number {
+     return (b:number):number => {
        return a - b;
      };
    },
    //incrementQuantity
-   incrementQuantity(id) {
+   incrementQuantity(this: ProductState, id:string){
      const item = this.user.cart.find(item => item.id === id);
      if (item) {
        item.quantity ++;
      }
    },
-   decrementQuantity(id) {
+   decrementQuantity(this: ProductState, id:string) {
     const item = this.user.cart.find(item => item.id === id);
     if (item &&  item.quantity > 1 ) {
       item.quantity --;
     }
   },
    // edit product
-   async updateProducts(id) {
+   async updateProducts(this: ProductState, id:string): Promise<void> {
      const product = this.products.find((p) => p.id === id);
      if (!product) return;
      try {
@@ -124,7 +124,7 @@ export const actions= {
      }
    },
    // get product by id
-   async getProducts(id) {
+   async getProducts(this: ProductState, id:string) {
      try {
        const { data } = await axios.get(`${baseUrl}/products/${id}`);
 
@@ -139,30 +139,22 @@ export const actions= {
        console.error("Updae ERROR:", err);
      }
    },
-   monthlyPrice(price) {
+   monthlyPrice(price:number):number {
      let totalPrice = Math.round(+price + (price * 40) / 100);
      let monthlyPrice = Math.round(totalPrice / 36);
      return monthlyPrice;
    },
 
-   dicountLabel(discount) {
-     if (discount < 9 && discount > 1) {
-       return true;
-     }
+   dicountLabel(discount:number):boolean  {
+    return !(discount <= 9 && discount >= 1);
    },
-   hugeSaleLabel(discount) {
-     if (discount < 100 && discount > 9) {
-       return true;
-     }
+   hugeSaleLabel(discount:number):boolean {
+    return !(discount <= 100 && discount >= 9);
    },
-   showPrices(discount) {
-     if (discount < 1 || discount > 99) {
-       return true;
-     }
+   showPrices(discount:number):boolean {
+    return !(discount >= 1 && discount <= 99);
    },
-   showOnePrice(discount) {
-     if (discount < 1 || discount > 99) {
-       return true;
-     }
+   showOnePrice(discount:number):boolean {
+     return !(discount >= 1 && discount <= 99);
    },
  }
