fb8b32e2bbae3bcfaca39a71b22128f96edf25eb
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-05-09 16:35:16 +0300

    add new logo, add new logo to brand store, add brand store logo to product store

--- frontend/src/sections/TheItem.vue
+++ frontend/src/sections/TheItem.vue
@@ -1,19 +1,15 @@
 <template>
   <!--Product item wrappper-->
   <div class="item__item" :class="{ gray: !stock }">
+    <!-- logo & labels -->
     <div class="item__top">
       <div class="item__top-logo">
-        <router-link :to="'/brands/' + brand" class="body-text">
-          <img
-      v-if="brandLogo"
-      :src="brandLogo"
-      :alt="product.brand"
-      class="brand-logo"
-    /> 
+        <router-link :to="'/brands/' + brand"  :class="{ grayscale: !stock }">
+          <img v-if="brandLogo" :src="brandLogo" :alt="product.brand" />
         </router-link>
       </div>
       <!-- labels for dicsount products -->
-      <div class="item__top-labels" :class="{ grayscale: !stock }" >
+      <div class="item__top-labels" :class="{ grayscale: !stock }">
         <div class="item__top-label" :class="{ visible: hugeSaleLabel }">
           <img src="@/assets/img/sale.jpg" alt="oops!" />
         </div>
@@ -37,10 +33,13 @@
     <div class="item__price">
       <div class="item__price-value">
         <p class="body-text bold" :class="{ oldPrice: !hidden }">
-          {{ price }} lei 
+          {{ price }} lei
         </p>
         <p class="body-text bold bolt" :class="{ hidden: hidden }">
-           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+            <path
+              d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z" />
+          </svg>
           {{ discountPrice }} lei
         </p>
       </div>
@@ -49,7 +48,7 @@
           Reducere <span class="bold">{{ discount }}%</span>
         </p>
         <p class="body-text-green">
-         - <span class="bold">{{savedMoney}} lei</span>
+          - <span class="bold">{{ savedMoney }} lei</span>
         </p>
       </div>
     </div>
@@ -66,44 +65,23 @@
       </div>
       <div class="item__buttons-action">
         <!-- add item to cart -->
-        <button
-          class="button-svg"
-          @click="$emit('addToCard', product)"
-          
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#e8eaed"
-          >
+        <button class="button-svg" @click="$emit('addToCard', product)">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
             <path
-              d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"
-            />
+              d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
           </svg>
         </button>
         <!-- add item to favorite -->
-        <button
-          class="button-svg"
-          @click="$emit('addToFavorite', product)"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#e8eaed"
-          >
+        <button class="button-svg" @click="$emit('addToFavorite', product)">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
             <path
-              d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
-            />
+              d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" />
           </svg>
         </button>
       </div>
     </div>
   </div>
-</template> 
+</template>
 
 <script setup lang="ts">
 import { defineOptions, onMounted } from 'vue';
@@ -116,7 +94,7 @@ defineOptions({
 import { useBrandsStore } from "@/stores/brands";
 import { computed } from "vue";
 
-import  type { Product} from '@/models/product'
+import type { Product } from '@/models/product'
 
 //emits
 const emit = defineEmits<{
@@ -125,12 +103,12 @@ const emit = defineEmits<{
 }>()
 
 //props
-const props = withDefaults(defineProps <{
+const props = withDefaults(defineProps<{
   id: string
   img: string
   product: Product
   productName: string
-  type:string,
+  type: string,
   price: number
   discount: number
   savedMoney: number
@@ -154,7 +132,7 @@ const brandLogo = computed(() => {
   const brand = brandsStore.brands.find(
     (b) => b.brand.toLowerCase() === props.product.brand.toLowerCase()
   );
-  return brand ? brand.img: null;
+  return brand ? brand.img : null;
 });
 
 
