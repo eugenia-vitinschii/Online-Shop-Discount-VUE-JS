583e51fbeb7e763bb309df023d6ea62c9de34438
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-05-22 14:57:52 +0300

    edit header button, scss rules

--- frontend/src/layout/TheHeader.vue
+++ frontend/src/layout/TheHeader.vue
@@ -23,29 +23,26 @@
               </router-link>
             </div>
             <div class="header__user">
-             <router-link class="header__link tooltip" to="/login" 
-             
+            <router-link 
+            v-if="!authStore.token"
+            class="header__link tooltip" 
+            to="/login" 
+            >
+                <span class="tooltip-text small-text">Login</span>
+                <svg 
+                class="menu-svg"
+                xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80Zm-360-80q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z"/></svg>              </router-link>
+
+             <router-link 
+             v-else="logout()"
+             class="header__link tooltip" 
+             to="/" 
              >
-                <span class="tooltip-text small-text"> user </span>
-              <svg v-if="!authStore.token"
-              class="menu-svg" 
-              xmlns="http://www.w3.org/2000/svg"
-               height="24px" 
-               viewBox="0 -960 960 960" 
-               width="24px" fill="#ffffff">
-               <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/>
-              </svg>
-              <button  v-else="logout()">
-              <svg     
-              class="menu-svg" 
-              xmlns="http://www.w3.org/2000/svg"
-               height="24px" 
-               viewBox="0 -960 960 960" 
-               width="24px" fill="#000">
-               <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/>
-              </svg>
+              <button  @click="logout">
+               <svg 
+               class="menu-svg"
+               xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M640-520v-80h240v80H640Zm-280 40q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z"/></svg>
               </button>
-                <span class="red__circle">l </span>
               </router-link>
               <router-link class="header__link tooltip" to="/shopping-cart">
                 <span class="tooltip-text small-text"> coșul </span>
