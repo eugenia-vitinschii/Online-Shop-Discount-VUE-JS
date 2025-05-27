671695aa6ac2d25cea6904b5cec0b138bd59acbc
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-05-27 15:31:55 +0300

    create scrollToTop component, create scss rules for button, impliment to app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <div>
 <div :class="['wrapper', { 'wrapper--promo': isPromoPage }]">
     <the-header />
   <div class="content">
   <router-view></router-view>
   </div>
   <the-footer />
-  <scroll-back />
+<scroll-to-top/>
 </div>
 
-
-  </div>
 </template>
 <script setup lang="ts"> 
 //vue
@@ -22,7 +19,7 @@ import { useRoute } from 'vue-router';
 //components
 import TheFooter from "./layout/TheFooter.vue";
 import TheHeader from "./layout/TheHeader.vue";
-import ScrollBack from "./layout/ScrollBack.vue";
+import ScrollToTop from './layout/ScrollToTop.vue';
 
 //varuables
 const route = useRoute();
---
+++ frontend/src/layout/ScrollToTop.vue
@@ -0,0 +1,43 @@
+<template>
+  <Transition name="fade">
+    <button
+      class="scrollToTop"
+      v-if="isVisible"
+      @click="scrollToTop"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#fff"
+        stroke-width="1" stroke-linecap="square">
+        <path d="M18 15l-6-6-6 6" />
+      </svg>
+    </button>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+//vue 
+import {ref} from 'vue'
+
+//hooks
+import { onMounted, onBeforeUnmount } from 'vue';
+
+//variables
+const isVisible = ref(false)
+
+const handleScroll = () => {
+  isVisible.value = window.scrollY > 350
+}
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth'})
+}
+
+//hooks
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
+</script>
