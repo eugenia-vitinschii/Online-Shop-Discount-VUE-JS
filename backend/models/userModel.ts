5599159cc172d93c70e6360422d41ed33cf97749
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2025-05-28 19:31:31 +0300

    edit userModel.ts, add new route

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,8 +1,9 @@
 // models/userModel.ts
 
 export interface User {
-  id: string;
+  id: string | number;
   name: string;
   password: string;
+  email: string;
   role: 'admin' | 'user';
 }
--- backend/routes/adminRoutes.ts
+++ backend/routes/adminRoutes.ts
@@ -3,11 +3,13 @@
 import  express from 'express';
 import { Request, Response, NextFunction } from 'express';
 import { authenticateToken, authorizeAdmin } from '../middlewares/authMiddleware';
-import {login} from '../controllers/authController'
+import {login, register} from '../controllers/authController'
+
 
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/register', register)
 
 router.get('/admin', authenticateToken, authorizeAdmin, ( req: Request, res: Response, next: NextFunction):void => {
   res.json({ message: 'Welcome to the admin panel!'});
